1404eec55f6aa52da956de63dc63e36835f97bc4
HomePC <PC@PC.com>
2026-02-03 17:04:14 +0200

    config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'project2',
+        'dbname' => 'financeapp',
         'charset' => 'utf8mb4'
     ]
 ];
